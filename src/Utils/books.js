e34003c9ae75b1e3a7b028e946a7d6dba7bdedc1
riliapuw <rinaapril1504@gmail.com>
2025-04-16 18:59:51 +0700

    Upload tugas 3

---
+++ src/Utils/books.js
@@ -0,0 +1,84 @@
+const books = [
+    {
+        id: 1,
+        title: "Ketika Laut Bercerita",
+        author: "Leila S. Chudori",
+        year: 2022,
+        description : "Ketika Laut Bercerita is a novel by Leila S. Chudori that tells the story of a student activist named Biru Laut who disappeared during the New Order era because of his struggle against injustice.",
+        image: "https://down-id.img.susercontent.com/file/id-11134207-7qul9-ljoz0m0j56ay04"
+    },
+
+    {
+        id: 2,
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        year: 1988,
+        description : "The Alchemist is a novel by Paulo Coelho that tells the story of Santiago, a young man who discovers a secret treasure that allows him to become a legendary hero.",
+        image: "https://i.pinimg.com/originals/e2/61/19/e26119b54502bc1fee47cb9f2ce14a84.jpg"
+    },
+
+    {   
+        id: 3,
+        title: "The Great Gatsby",        
+        author: "F. Scott Fitzgerald",
+        year: 1925,
+        description : "The Great Gatsby is a novel by F. Scott Fitzgerald that tells the story of Jay Gatsby, a millionaire and socialite who is obsessed with the American Dream. He is caught up in a world of greed and corruption.",
+        image: "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/98/MTA-163146815/no-brand_no-brand_full01.jpg"
+    },
+    
+    {
+        id: 4,
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960,
+        description : "To Kill a Mockingbird is a novel by Harper Lee that tells the story of Atticus Finch, a man who defends a black man in a white-only society.",
+        image: "https://tse2.mm.bing.net/th?id=OIP.5ZCbv7QNTZ7MdtiZvp4RrgHaEK&pid=Api&P=0&h=180"
+    },          
+
+    {
+        id: 5,
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        year: 1951,
+        description : "The Catcher in the Rye is a novel by J.D. Salinger that tells the story of a teenage boy named Holden Caughlan in a small town.",
+        image: "https://i0.wp.com/www.raptisrarebooks.com/images/111590/the-catcher-in-the-rye-jd-salinger-first-edition-1951-rare.jpg?fit=1000%2C800&ssl=1"
+    },  
+
+    {
+        id: 6,
+        title: "Pride and Prejudice",
+        author: "Jane Austen",
+        year: 1813,
+        description : "Pride and Prejudice is a novel by Jane Austen that tells the story of Elizabeth Bennet, a young woman who is forced to live with her wealthy aunt.",
+        image: "https://down-id.img.susercontent.com/file/id-11134201-23020-r7kbuaqg3tnvf2"
+    },      
+
+    {
+        id: 7,
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        year: 1937,
+        description : "The Hobbit is a novel by J.R.R. Tolkien that tells the story of Bilbo Baggins, a hobbit who is taken on a journey to the Lonely Mountain.",
+        image: "https://4.bp.blogspot.com/-_fUp4Fnnr4M/Xo6BEqj0EDI/AAAAAAAAhyg/JTiW0MMGu2sWp_iZL29600XRGy5A78fwgCLcBGAsYHQ/w1200-h630-p-k-no-nu/the-hobbit-jrr-tolkien.jpg"
+    },  
+
+    {
+        id: 8,
+        title: "Romeo and Juliet",
+        author: "William Shakespeare",
+        year: 1597,
+        description : "Romeo and Juliet is a tragedy by William Shakespeare that tells the story of Romeo, a young man who falls in love with Juliet, a young woman.",
+        image: "https://down-id.img.susercontent.com/file/id-11134207-7r98o-loo3vizglzajaa"
+    },
+
+    {
+        id: 9,
+        title: "Brave New World",
+        author: "Aldous Huxley",
+        year: 1932,
+        description : "Brave New World is a dystopian novel by Aldous Huxley that tells the story of a future world where humanity is controlled by a totalitarian government.",
+        image: "https://tse1.mm.bing.net/th?id=OIP.WtMnVynK38o1-m2pz2bgogHaF7&pid=Api&P=0&h=180"
+    }
+];
+
+export default books;
--- src/components/shared/ProductList/index.jsx
+++ src/components/shared/ProductList/index.jsx
@@ -1,6 +1,21 @@
 import { Link } from "react-router";
+import books from "../../../Utils/books";
+import { useState } from "react";
 
 export default function ProductList() {
+  const [bookList, setBookList] = useState(books);
+
+  const handleAddBook = () => {
+    const newBook = {
+      id: bookList.length + 1,
+      title: "The Silent Patient",
+      author: "Alex Michaelides",
+      year: 2019,
+      description: "The Silent Patient is a psychological thriller that follows Alicia Berenson, a famous painter who shoots her husband and then goes silent.",
+      image: "https://tse3.mm.bing.net/th?id=OIP.iL37ud0WaU6GyZB3i_fPPAHaFT&pid=Api&P=0&h=180"
+    };
+    setBookList([...bookList, newBook]);
+  }
   return (
     <>
       <section className="py-5 text-center container mb-10" id="book">
@@ -18,148 +33,34 @@ export default function ProductList() {
 
       <div className="album py-5 bg-body-tertiary mb-10">
         <div className="container">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
-          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" id="detail">
-            <div className="col">
-              <div className="card shadow-sm">
-              <img src="https://down-id.img.susercontent.com/file/id-11134207-7qul9-ljoz0m0j56ay04" className="card-img-top" alt="Ketika Laut Bercerita" width="100%" height="225" style={{ objectFit: 'cover' }} />
-              <div className="card-body">
-                  <p className="card-text">Laut Bercerita tells the story of a student activist who disappeared during Indonesia’s authoritarian era, filled with pain and silent resistance.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-body-secondary">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card shadow-sm">
-              <img src="https://down-id.img.susercontent.com/file/4b4efcd98e5c438de7de2b07bd60c0d0" className="card-img-top" alt="Ketika Laut Bercerita" width="100%" height="225" style={{ objectFit: 'cover' }} />
+            {bookList.map((book) => (
+              <div key={book.id} className="col">
+                <div className="card shadow-sm">
+                <img src={book.image} className="card-img-top" alt={book.title} width="100%" height="225" style={{ objectFit: 'cover' }} />
                 <div className="card-body">
-                  <p className="card-text">Matahari Minor is a journey of self-discovery and spirituality set in the philosophical Supernova universe.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                    <h5 className="card-title">{book.title}</h5>
+                    <p className="card-text">
+                      {book.description}
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <p>{book.author}</p>
+                      </div>
+                      <small className="text-body-secondary">{book.year}</small>
                     </div>
-                    <small className="text-body-secondary">9 mins</small>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col">
-              <div className="card shadow-sm">
-              <img src="https://down-id.img.susercontent.com/file/id-11134207-7r98u-lpj4j1ndmv8818" className="card-img-top" alt="Ketika Laut Bercerita" width="100%" height="225" style={{ objectFit: 'cover' }} />
-              <div className="card-body">
-                  <p className="card-text">ILY captures the purity of first love and the pain of letting go. It’s a nostalgic story about growing up and moving on.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-body-secondary">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col">
-              <div className="card shadow-sm">
-              <img src="http://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1451905281i/28446637._UY630_SR1200,630_.jpg" className="card-img-top" alt="Ketika Laut Bercerita" width="100%" height="225" style={{ objectFit: 'cover' }} />
-              <div className="card-body">
-                  <p className="card-text">Hujan tells of a girl’s emotional recovery after a catastrophic disaster changes her world. Through trauma and hope, she rediscovers strength and purpose.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-body-secondary">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card shadow-sm">
-              <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1552044331i/44304793._UY630_SR1200,630_.jpg" className="card-img-top" alt="Ketika Laut Bercerita" width="100%" height="225" style={{ objectFit: 'cover' }} />
-                <div className="card-body">
-                  <p className="card-text">Komet Minor continues the fantasy adventure in the "Bumi" universe, exploring new worlds and challenges.It’s a moving tale of grief, healing, and choosing to move forward.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-body-secondary">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card shadow-sm">
-              <img src="https://lzd-img-global.slatic.net/g/p/f14f403d9a62e0e3b51fe947e7ab2c73.jpg_720x720q80.jpg" className="card-img-top" alt="Ketika Laut Bercerita" width="100%" height="225" style={{ objectFit: 'cover' }} />
-                <div className="card-body">
-                  <p className="card-text">Tentang Kamu tells the story of a young lawyer who unravels the mysterious life of Sri Ningsih, a humble woman with a remarkable past filled with love, sacrifice, and resilience.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-body-secondary">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col">
-              <div className="card shadow-sm">
-              <img src="https://down-id.img.susercontent.com/file/2b95491667bab776d83d04adfa27b11c" className="card-img-top" alt="Ketika Laut Bercerita" width="100%" height="225" style={{ objectFit: 'cover' }} />
-              <div className="card-body">
-                  <p className="card-text">Sesuk features humorous and wise tales of Bibi Gill, filled with warmth and life lessons.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-body-secondary">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card shadow-sm">
-              <img src="https://down-id.img.susercontent.com/file/id-11134207-7rasg-m3q1xj2dmmj480" className="card-img-top" alt="Ketika Laut Bercerita" width="100%" height="225" style={{ objectFit: 'cover' }} />
-                <div className="card-body">
-                  <p className="card-text">Aldebaran presents a spiritual journey in search of truth and purpose, wrapped in inspirational fiction.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-body-secondary">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card shadow-sm">
-              <img src="https://down-id.img.susercontent.com/file/id-11134207-7qula-lfoutmlpijgh71" className="card-img-top" alt="Ketika Laut Bercerita" width="100%" height="225" style={{ objectFit: 'cover' }} />
-                <div className="card-body">
-                  <p className="card-text">Helo takes readers into the fantasy realm of the Para Leluhur, full of mystery and thrilling quests.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-body-secondary">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
+            ))}
+            
           </div>
         </div>
       </div>
+      <button onClick={handleAddBook} className="btn btn-success mt-3">
+        Tambah Buku Baru
+      </button>
     </>
   )
 }
